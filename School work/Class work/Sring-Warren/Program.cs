58fd6e165757c17d8c7954133f6a2abc1e35a1c1
warrenh123 <huw@stpaulsschool.org.uk>
2025-10-01 21:22:12 +0100

    school work

--- School work/Class work/Sring-Warren/Program.cs
+++ School work/Class work/Sring-Warren/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("String work");
+﻿//Exercise 1
+static void waveFunction(string userInput)
+{
+    for (int i = 0; i < userInput.Length; i++)
+    {
+        if (userInput.Substring(i, 1).Equals(" ")) continue;
+        else
+        {
+            string x = userInput.Substring(i, 1).ToUpper();
+            for (int a = 0; a < userInput.Length; a++)
+            {
+                if (a == i) Console.Write(x);
+                else Console.Write(userInput.Substring(a, 1)); 
+            }
+            Console.Write(" ");
+        }
+
+    }
+    Console.WriteLine(" ");
+}
+waveFunction("hello");
+
+//Exercise 2
+string word = "hello 你好";
+foreach (char c in word)
+{
+    int x = (int)c;
+    Console.Write(x + " ");
+}
+
